3e7ebd395a1bb8e383438421553913d291d9c920
Alexander Drapoy <dr4alex@gmail.com>
2022-12-02 20:40:23 +0200

    fix bag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,13 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter.jsx';
 import { ContactList } from './ContactList/ContactList';
 import { AppContainer } from './App.styled';
+import { useRef } from 'react';
 
 export const App = () => {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
+
+  const isFirstRender = useRef(true);
 
   // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
@@ -31,12 +34,16 @@ export const App = () => {
   useEffect(() => {
     const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
     console.log(parsedContacts);
-    if (parsedContacts.length > 0) {
+    if (parsedContacts) {
       setContacts(parsedContacts);
     }
   }, []);
   useEffect(() => {
-    contacts.length > 0 &&
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    contacts.length >= 0 &&
       localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
@@ -54,9 +61,9 @@ export const App = () => {
           <Filter filter={filter} onChangeValue={filterContacts} />
 
           <ContactList
+            onDeleteContact={deleteContact}
             contacts={contacts}
             filter={filter}
-            onDeleteContact={deleteContact}
           />
         </div>
       )}
@@ -64,82 +71,7 @@ export const App = () => {
   );
 };
 
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-
-//     filter: '',
-//   };
-
-//   filterContacts = e => {
-//     const { value } = e.target;
-//     this.setState({
-//       filter: value.toLowerCase(),
-//     });
-//   };
-//   onSubmitForm = ({ name, number }) => {
-//     if (this.state.contacts.find(el => el.name === name)) {
-//       alert(`${name} is alredy in contacts`);
-//       return;
-//     }
-//     this.setState(prevState => ({
-//       contacts: [
-//         ...prevState.contacts,
-//         {
-//           id: nanoid(),
-//           name: name,
-//           number: number,
-//         },
-//       ],
-//     }));
-//   };
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.contacts !== prevState.contacts) {
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   componentDidMount() {
-//     const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
-
-//   deleteContact = contactId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//     }));
-//   };
-//   render() {
-//     return (
-//       <AppContainer>
-//         <h1>Phonebook</h1>
-//         <ContactForm onSubmit={this.onSubmitForm}></ContactForm>
-//         {this.state.contacts.length > 0 && (
-//           <div>
-//             <h2>Contacts</h2>
-//             <Filter
-//               filter={this.state.filter}
-//               onChangeValue={this.filterContacts}
-//             />
-
-//             <ContactList
-//               contacts={this.state.contacts}
-//               filter={this.state.filter}
-//               onDeleteContact={this.deleteContact}
-//             />
-//           </div>
-//         )}
-//       </AppContainer>
-//     );
-//   }
-// }
+//
 
 App.propTypes = {
   onSubmit: PropTypes.func,
